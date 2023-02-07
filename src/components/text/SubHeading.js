9729b349aa5812624225e23aed501c0d178661b1
shrCreation <shrhossain786@gmail.com>
2023-02-08 03:18:48 +0600

    feat: featured category done

--- src/components/text/SubHeading.js
+++ src/components/text/SubHeading.js
@@ -2,10 +2,14 @@ import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import Fonts from '../../constants/Fonts';
 
-const SubHeading = ({children, color, size}) => {
+const SubHeading = ({children, color, size, textAlign}) => {
   return (
     <View>
-      <Text style={[styles.subHeading, {color: color, fontSize: size}]}>
+      <Text
+        style={[
+          styles.subHeading,
+          {color: color, fontSize: size, textAlign: textAlign},
+        ]}>
         {children}
       </Text>
     </View>
@@ -16,7 +20,6 @@ export default SubHeading;
 
 const styles = StyleSheet.create({
   subHeading: {
-    textAlign: 'center',
     marginTop: 5,
     lineHeight: 24,
     fontFamily: Fonts.InterRegular,
--- src/views/home/Featured.js
+++ src/views/home/Featured.js
@@ -1,5 +1,12 @@
-import {StyleSheet, Text, View, ScrollView} from 'react-native';
-import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+  FlatList,
+} from 'react-native';
+import React, {useState} from 'react';
 import Colors from '../../constants/Colors';
 import Fonts from '../../constants/Fonts';
 import Images from '../../constants/Images';
@@ -8,13 +15,55 @@ import SubHeading from '../../components/text/SubHeading';
 import Header from '../../components/util/Header';
 import HomeSlider from '../../components/slider/HomeSlider';
 import PromoDialog from '../../components/promotions/PromoDialog';
+import Categories from '../../components/list/Categories';
+import Data from '../../constants/Data';
+import TabBtn from '../../components/button/TabBtn';
 
 const Featured = () => {
+  const [selectedId, setSelectedId] = useState();
+
+  const renderItem = item => {
+    const backgroundColor =
+      item.id === selectedId ? Colors.primary : Colors.lightPink;
+    const color = item.id === selectedId ? 'white' : Colors.gray;
+    return (
+      <TabBtn
+        data={item}
+        backgroundColor={backgroundColor}
+        color={color}
+        onPress={() => setSelectedId(item.id)}
+      />
+    );
+  };
   return (
     <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
       <Header />
       <PromoDialog>Future-ready skills on your schedule</PromoDialog>
       <HomeSlider />
+      <View style={styles.contentView}>
+        <Heading color={Colors.black} size={28} textAlign={'left'}>
+          Learning that fits
+        </Heading>
+        <SubHeading color={Colors.darkGray} size={15} textAlign={'left'}>
+          Skills for your present(and future)
+        </SubHeading>
+        <View style={styles.catView}>
+          <Heading color={Colors.black} size={22} textAlign={'left'}>
+            Categories
+          </Heading>
+          <TouchableOpacity activeOpacity={0.5}>
+            <Text style={styles.catText}>See all</Text>
+          </TouchableOpacity>
+        </View>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          <FlatList
+            data={Data}
+            keyExtractor={item => item.id}
+            numColumns={Math.ceil(Data.length / 2)}
+            renderItem={({item}) => renderItem(item)}
+          />
+        </ScrollView>
+      </View>
     </ScrollView>
   );
 };
@@ -26,5 +75,23 @@ const styles = StyleSheet.create({
     flex: 1,
     marginBottom: 65,
     backgroundColor: Colors.light,
+    marginBottom: 65,
+  },
+  contentView: {
+    marginLeft: 20,
+    marginVertical: 20,
+  },
+  catView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 15,
+    marginBottom: 15,
+  },
+  catText: {
+    marginRight: 15,
+    color: Colors.blue,
+    fontSize: 13,
+    fontFamily: Fonts.InterBold,
   },
 });
